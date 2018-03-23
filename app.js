4ad47a432db2a4176550eaa3519d8d0c235fdd5d
cplayer <zhcplayer@gmail.com>
2018-03-23 21:17:26 +0800

    fix encoding and passwd for database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "testPasswd",
+    password: "12345678",
     database: "serverDB"
 });
 
